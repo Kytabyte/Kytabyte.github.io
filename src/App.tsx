3321acbe36c0e12e9b832fc3b9461d74fdedbc30
Kytabyte <y69xie@uwaterloo.ca>
2020-10-09 16:22:02 -0400

    try browser router with PUBLIC_URL

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Blog from './components/Blog';
 import Home from './components/Home'
 import Project from './components/Project';
 
+const makeurl = (url: string) => {
+  return process.env.PUBLIC_URL + url;
+}
+
+
 class App extends React.Component<object, object> {
   componentDidMount() {
     document.title = "Kytabyte's Home";
@@ -13,15 +18,9 @@ class App extends React.Component<object, object> {
     return (
       <>
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/blog">
-            <Blog />
-          </Route>
-          <Route path="/project">
-            <Project />
-          </Route>
+          <Route exact path={makeurl("/")} component={Home} />
+          <Route path={makeurl("/blog")} component={Blog} />
+          <Route path={makeurl("/project")} component={Project} />
         </Switch>
       </>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
